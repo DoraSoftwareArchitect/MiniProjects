680de7fb6693960bf94401293fbf22be09871e3a
vignesh <doraashokit24@gmail.com>
2025-02-11 11:33:25 +0530

    cOMPLETED Councellors Application project with no errors

---
+++ src/main/java/com/dora/CouncellorApplication.java
@@ -0,0 +1,12 @@
+package com.dora;
+
+
+public class CouncellorApplication {
+
+	public static void main(String[] args) {
+		System.out.println("Application has been started sucessfully,,,,");
+		
+	}
+	
+
+}
